8bcb884e880380f5d8db74051edd6d0bdb7bbcf9
Daniel alvarez <montalvandaniel2@gmail.com>
2019-01-31 15:50:29 -0600

    3
    Get categorias

--- src/Componentes/App.js
+++ src/Componentes/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import Header from './Header'
 import '../CSS/App.css';
 class App extends Component {
+  state = {
+    noticias: []
+  };
+  componentDidMount() {
+    this.getCategorias();
+  }
+  getCategorias = () => {
+
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=6DAUMMSOKET7RPW7IHEN`;
+
+    fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(categorias => {
+        console.log(categorias.categories)
+        console.log(categorias.categories)
+      });
+  };
   render() {
     return (
       <div className="App">
